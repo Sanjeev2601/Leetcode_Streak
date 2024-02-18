d0e4f0e750e3cd8d8cd7902b3cf5dbfc1dde1208
Sanjeev2601 <89632293+Sanjeev2601@users.noreply.github.com>
2024-02-18 13:35:38 -0500

    Time: 4 ms (42.39%) | Memory: 43.2 MB (7.15%) - LeetSync

--- 293-flip-game/flip-game.java
+++ 293-flip-game/flip-game.java
@@ -4,7 +4,7 @@ class Solution {
         int n = currentState.length();
         for(int i = 0; i < n - 1; i++){
             if(currentState.charAt(i) == '+' && currentState.charAt(i + 1) == '+') {
-                String temp = currentState.substring(0, i) + "--" + currentState.substring(i + 2, n);
+                String temp = currentState.substring(0, i) + "--" + currentState.substring(i + 2);
                 ans.add(temp);
             }
         }
