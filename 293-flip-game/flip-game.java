113bf0b4ff34df7782c4b6c4948830f179459bc7
Sanjeev2601 <89632293+Sanjeev2601@users.noreply.github.com>
2024-02-18 13:35:27 -0500

    Time: 4 ms (42.39%) | Memory: 42.6 MB (52.72%) - LeetSync

---
+++ 293-flip-game/flip-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> generatePossibleNextMoves(String currentState) {
+        List<String> ans = new ArrayList<>();
+        int n = currentState.length();
+        for(int i = 0; i < n - 1; i++){
+            if(currentState.charAt(i) == '+' && currentState.charAt(i + 1) == '+') {
+                String temp = currentState.substring(0, i) + "--" + currentState.substring(i + 2, n);
+                ans.add(temp);
+            }
+        }
+        return ans;
+    }
+}
