a65e20e450db2229039fb6d92e404dbdbb1faf51
Sanjeev2601 <89632293+Sanjeev2601@users.noreply.github.com>
2024-02-22 12:41:16 -0500

    Time: 25 ms (19.66%) | Memory: 48 MB (30.06%) - LeetSync

---
+++ 1177-tree-diameter/tree-diameter.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int treeDiameter(int[][] edges) {
+        List<Set<Integer>> graph = new ArrayList<Set<Integer>>();
+        for (int i = 0; i < edges.length + 1; ++i) {
+            graph.add(new HashSet<Integer>());
+        }
+        for (int[] edge : edges) {
+            Integer u = edge[0], v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        int[] nodeDistance = bfs(0, graph);
+        nodeDistance = bfs(nodeDistance[0], graph);
+        return nodeDistance[1];
+    }
+
+    private int[] bfs(int start, List<Set<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size()];
+        visited[start] = true;
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.addLast(start);
+        Integer lastNode = start, distance = -1;
+        while (queue.size() > 0) {
+            LinkedList<Integer> nextQueue = new LinkedList<Integer>();
+            while (queue.size() > 0) {
+                int nextNode = queue.removeFirst();
+                for (Integer neighbor : graph.get(nextNode)) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        nextQueue.addLast(neighbor);
+                        lastNode = neighbor;
+                    }
+                }
+            }
+            distance += 1;
+            queue = nextQueue;
+        }
+        return new int[] {lastNode, distance};
+    }
+}
