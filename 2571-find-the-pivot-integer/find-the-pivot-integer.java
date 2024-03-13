8b78fc8fd2ff6319e658f45c85a30dd6937d4bbd
Sanjeev2601 <89632293+Sanjeev2601@users.noreply.github.com>
2024-03-13 13:47:11 -0400

    Time: 9 ms (17.91%) | Memory: 44.5 MB (5.03%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,33 +1,5 @@
 class Solution {
     public int pivotInteger(int n) {
-        // Stack<Integer> left = new Stack<Integer>();
-        // Stack<Integer> right = new Stack<Integer>();
-        // Map<Integer, Integer> hm = new HashMap<>();
-        // int leftsum = 0, rightsum = 0;
-        // if(n == 1){
-        //     return 1;
-        // }
-        // for(int i = 1, j = n; i < n; i++, j--){
-        //     leftsum += i;
-        //     rightsum += j;
-        //     left.push(leftsum);
-        //     right.push(rightsum);
-        //     hm.put(leftsum, i);
-        // }
-        // int i = 1;
-        // int ans = -1;
-        // while(i < n){
-        //     if(left.peek() < right.peek()){
-        //         right.pop();
-        //     }
-        //     else if(left.peek() > right.peek()){
-        //         left.pop();
-        //     }
-        //     else{
-        //         hm.get(left.peek());
-        //     }
-        // }
-        // return ans;
         HashMap<Integer, Integer> hm = new HashMap<>();
         int sum = 0;
         for(int i = 1; i <= n; i++){
