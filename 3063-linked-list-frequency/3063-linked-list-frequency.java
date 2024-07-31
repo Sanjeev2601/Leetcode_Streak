106963b24fa889054cc273d3109e1d5697018cbc
Sanjeev Parthasarathy <sanjeev.p2601@gmail.com>
2024-07-30 21:46:50 -0400

    Time: 27 ms (92.11%), Space: 63.5 MB (7.90%) - LeetHub

---
+++ 3063-linked-list-frequency/3063-linked-list-frequency.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode frequenciesOfElements(ListNode head) {
+        Map<Integer, ListNode> hm = new HashMap<>();
+        ListNode temp = head;
+        ListNode ans = null;
+        while(temp != null){
+            if(hm.containsKey(temp.val)){
+                ListNode freqNode = hm.get(temp.val);
+                freqNode.val++;
+            }
+            else{
+                ListNode newFreqNode = new ListNode(1, ans);
+                hm.put(temp.val, newFreqNode);
+                ans = newFreqNode;
+            }
+            temp = temp.next;
+        }
+        return ans;
+    }
+}
