52f3473ef045a90f1dc6a53da933d0eeef636c30
Sanjeev Parthasarathy <sanjeev.p2601@gmail.com>
2024-07-30 21:27:35 -0400

    Time: 1 ms (69.28%), Space: 44 MB (75.82%) - LeetHub

---
+++ 3062-winner-of-the-linked-list-game/3062-winner-of-the-linked-list-game.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public String gameResult(ListNode head) {
+        ListNode even = head;
+        ListNode odd = head;
+        int oc = 0, ec = 0;
+        while(even != null){
+            odd = even.next;
+            if(even.val > odd.val){
+                ec++;
+            }
+            else{
+                oc++;
+            }
+            even = odd.next;
+        }
+        if(oc > ec){
+            return "Odd";
+        }
+        else if(oc == ec){
+            return "Tie";
+        }
+        return "Even";
+    }
+}
