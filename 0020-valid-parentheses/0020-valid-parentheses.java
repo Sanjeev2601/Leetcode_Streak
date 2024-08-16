e8a1a043252eff3b017babe9de9dfb21e54a2f10
Sanjeev Parthasarathy <sanjeev.p2601@gmail.com>
2024-08-16 19:38:03 -0400

    Time: 1 ms (98.80%), Space: 41.4 MB (48.80%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n % 2 != 0){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(stack.isEmpty() || ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }
+            else{
+                if((stack.peek() == '(' && ch == ')') ||
+                (stack.peek() == '[' && ch == ']') ||
+                (stack.peek() == '{' && ch == '}')){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
